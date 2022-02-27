af4b3150960eb9dedf78f6e34a84fa9ff2d99774
Melissa Jeremie <melissajeremie@gmail.com>
2022-02-26 20:44:42 -0500

    js variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const currentInput
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousInputText= document.querySelector('[data-previous-input]');
+const currentInputText= document.querySelector('[data-current-input]');
