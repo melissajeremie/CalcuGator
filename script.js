a567d3cf930376f69860551121b07851491f27b3
Melissa Jeremie <melissajeremie@gmail.com>
2022-02-26 20:57:23 -0500

    calculator class

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const currentInput
+class Calculator {
+    constructor( previousInputText, currentInputText) {
+        this.previousInputText = previousInputText
+        this.currentInputText = currentInputText
+        this.clear()
+    }
+
+    allClear(){
+        this.currentInput = ''
+        this.previousInput = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
 
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operation]');
@@ -7,3 +39,5 @@ const clearButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousInputText= document.querySelector('[data-previous-input]');
 const currentInputText= document.querySelector('[data-current-input]');
+
+const calculator = new Calculator(previousInputText, currentInputText)
